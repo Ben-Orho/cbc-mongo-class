c3af63262b044b90818545c410da660cb52e6b42
Ben-Orho <heyjis4connection@gmail.com>
2022-08-24 23:57:24 +0100

    added more route

--- Routes/auth.route.js
+++ Routes/auth.route.js
@@ -1,12 +1,8 @@
-const express = require('express')
-const { signupController } = require('../Controllers/auth.controller')
-const router = express.Router()
+const express = require("express");
+const { signupController } = require("../Controllers/auth.controller");
+const router = express.Router();
 
+router.post("/signup", signupController);
+router.get("/login");
 
-router.post('/signup', signupController)
-router.get('/login',)
-
-
-module.exports = router
-
-
+module.exports = router;
